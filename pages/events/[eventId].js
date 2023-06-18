0cef618bf178ee2a152a1a4fea16b6c3c6f62ded
kvasquez123 <kvasquez@qantamedia.com>
2023-06-17 21:09:49 -0400

    feat: add componentes for details events

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
 const EventId = () => {
-  return (
-    <div>EventId</div>
-  )
-}
+    const router = useRouter();
+    const { eventId } = router.query;
+    const event = getEventById(eventId);
+    if (!event) {
+        return <p>No event found</p>;
+    }
+    // conso
+    return <div>EventId</div>;
+};
 
-export default EventId
+export default EventId;
